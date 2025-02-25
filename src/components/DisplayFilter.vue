6efb1037f452329dc0cbf196efe599d740d7bf8a
radiantly <buildingsomethingfun@gmail.com>
2025-02-25 19:28:32 +0530

    Add filter apply button

--- src/components/DisplayFilter.vue
+++ src/components/DisplayFilter.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, watch } from "vue";
 import { manager } from "../globals";
+import FilterApplyIcon from "./icons/FilterApplyIcon.vue";
 
 const isFilterValid = ref(null);
 const displayFilterInput = ref("");
@@ -39,6 +40,17 @@ const handleSubmit = () => {
               : 'var(--ws-ugly-red)',
         }"
       />
+      <button
+        type="submit"
+        class="apply-filter"
+        :class="{
+          disabled:
+            isFilterValid === false ||
+            manager.displayFilter === displayFilterInput,
+        }"
+      >
+        <FilterApplyIcon />
+      </button>
     </form>
     <!-- There's a button to add a display filter button. This feature is Not Planned -->
   </div>
@@ -64,5 +76,34 @@ const handleSubmit = () => {
   border: none;
   background-color: var(--ws-display-filter-bg);
   padding: 1px 4px;
+}
+.apply-filter {
+  border: none;
+  background: none;
+  padding: 0;
+  outline: none;
+
+  height: 14px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: center;
+  margin: 0 5px;
+  filter: brightness(1.75);
+  opacity: 0.9;
+}
+.apply-filter.disabled {
+  pointer-events: none;
+  filter: grayscale(100%);
+  opacity: 0.4;
+}
+.apply-filter:hover {
+  filter: brightness(1.25);
+}
+.apply-filter:active {
+  filter: brightness(1);
+}
+.apply-filter svg {
+  height: 100%;
 }
 </style>
---
+++ src/components/icons/FilterApplyIcon.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg viewBox="0 0 24 14">
+    <defs>
+      <linearGradient
+        id="a"
+        x1="8"
+        x2="15"
+        y2="14"
+        gradientTransform="translate(0 1038.4)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#fff" stop-opacity=".15753" offset="0" />
+        <stop stop-color="#fff" stop-opacity="0" offset="1" />
+      </linearGradient>
+    </defs>
+    <g transform="translate(0 -1038.4)" fill-rule="evenodd">
+      <rect
+        x="-26"
+        y="1038.4"
+        width="24"
+        height="14"
+        rx="2"
+        ry="2"
+        color="#000000"
+        fill="#3465a4"
+      />
+      <rect
+        x="-22"
+        y="1043.4"
+        width="14"
+        height="4"
+        rx="1"
+        ry="1"
+        color="#000000"
+        fill="#eeeeec"
+      />
+      <path
+        d="m-11 1042.4v6.0003l4.0001-3.0001z"
+        fill="#eeeeec"
+        stroke="#eeeeec"
+        stroke-linejoin="round"
+        stroke-width="2"
+      />
+      <path
+        transform="translate(0 1038.4)"
+        d="m2 0c-1.108 0-2 0.892-2 2v10c0 1.108 0.892 2 2 2h20c1.108 0 2-0.892 2-2v-10c0-1.108-0.892-2-2-2h-20zm13.043 3a1.0001 1.0001 0 0 1 0.55664 0.20117l4 3a1.0001 1.0001 0 0 1 0 1.5996l-4 3a1.0001 1.0001 0 0 1-1.5996-0.80078v-1h-9c-0.554 0-1-0.446-1-1v-2c0-0.554 0.446-1 1-1h9v-1a1.0001 1.0001 0 0 1 0.94336-0.99805 1.0001 1.0001 0 0 1 0.09961-0.0019531z"
+        color="#000000"
+        fill="#204a87"
+      />
+      <path
+        d="m2 1038.4c-1.108 0-2 0.892-2 2v10c0 1.108 0.892 2 2 2h20c1.108 0 2-0.892 2-2v-10c0-1.108-0.892-2-2-2h-20zm13.043 3a1.0001 1.0001 0 0 1 0.55664 0.2012l4 3a1.0001 1.0001 0 0 1 0 1.5996l-4 3a1.0001 1.0001 0 0 1-1.5996-0.8008v-1h-9c-0.554 0-1-0.446-1-1v-2c0-0.554 0.446-1 1-1h9v-1a1.0001 1.0001 0 0 1 0.94336-0.998 1.0001 1.0001 0 0 1 0.09961 0z"
+        color="#000000"
+        fill="url(#a)"
+      />
+    </g>
+  </svg>
+</template>
