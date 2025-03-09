8d91ae1784c9854cb9588cde4a8f4ca9bf1a4f96
radiantly <buildingsomethingfun@gmail.com>
2025-03-09 22:15:01 +0530

    Allow resizing of layout panes

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -1,14 +1,44 @@
+<script setup>
+import { reactive } from "vue";
+import { watchMouseDragMove } from "../../util";
+
+// TODO: Refactor resize code into reusable block
+// Can be done once we have more than a single layout
+
+const state = reactive({
+  firstPaneHeight: Math.round(window.innerHeight / 2),
+  secondPaneWidth: Math.round(window.innerWidth / 2),
+});
+
+const handleVResize = (event) => {
+  const originalHeight = state.firstPaneHeight;
+  const handleMouseDragMove = ({ deltaY }) =>
+    (state.firstPaneHeight = Math.max(0, originalHeight + deltaY));
+  watchMouseDragMove(event, handleMouseDragMove);
+};
+
+const handleHResize = (event) => {
+  const originalWidth = state.secondPaneWidth;
+  const handleMouseDragMove = ({ deltaX }) =>
+    (state.secondPaneWidth = Math.max(0, originalWidth + deltaX));
+  watchMouseDragMove(event, handleMouseDragMove);
+};
+</script>
+
 <template>
   <div class="fl layout-container">
-    <div class="fl top">
+    <div class="fl top" :style="{ height: state.firstPaneHeight + 'px' }">
       <slot name="slot1"></slot>
     </div>
-    <div class="v-resize"></div>
+    <div class="v-resize" @mousedown="handleVResize"></div>
     <div class="fl bottom">
-      <div class="fl first quarter needs-border-bottom">
+      <div
+        class="fl first quarter needs-border-bottom"
+        :style="{ width: state.secondPaneWidth + 'px' }"
+      >
         <slot name="slot2"></slot>
       </div>
-      <div class="h-resize"></div>
+      <div class="h-resize" @mousedown="handleHResize"></div>
       <div class="fl second quarter needs-border-bottom">
         <slot name="slot3"></slot>
       </div>
@@ -31,14 +61,13 @@
 }
 .top {
   flex-shrink: 0;
-
-  height: 50vh;
 }
 .v-resize {
   flex-shrink: 0;
 
   height: 4px;
   border-top: var(--ws-pane-border);
+  cursor: ns-resize;
 }
 .bottom {
   flex-grow: 1;
@@ -48,13 +77,13 @@
   border-bottom: none;
 }
 .first.quarter {
-  width: 50vw;
   border-left: none;
 }
 .h-resize {
   flex-shrink: 0;
 
   width: 4px;
+  cursor: ew-resize;
 }
 .second.quarter {
   flex-grow: 1;
