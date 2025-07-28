48ab78e0272defb20cb68ec3858703c871335988
jerryokk <126859008+jerryokk@users.noreply.github.com>
2025-07-28 16:34:49 +0000

    更新打包脚本

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        popup: resolve(__dirname, 'popup.html')
+      }
+    }
   },
 });
